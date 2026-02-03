aea981bba5fa337a4b02cad8a533dca60d58a0ff
sandram <sandra@measurable.energy>
2026-02-03 21:40:50 +0000

    further work

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Project
+from .forms import ProjectForm
 
 
 # Create your views here.
@@ -14,11 +15,42 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     tags = projectObj.tags.all()
-    
-
-            
     return render(request, "projects/single-project.html", {"project": projectObj, "tags": tags})   
 
+def createProject(request):
+    form = ProjectForm()
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {
+        "form": form
+    }
+    return render(request, "projects/project_form.html" , context)
 
+def updateProject(request, pk):
+    project = Project.objects.get(id=pk)
+    form = ProjectForm(instance=project)
 
+    if request.method == "POST":
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {
+        "form": form
+    }
+    return render(request, "projects/project_form.html" , context)
+        
+
+def deleteProject(request,pk):
+    project = Project.objects.get(id=pk)
+    context = {
+        "object": project
+    }
+    if request.method == "POST":
+        project.delete()
+        return redirect('projects') 
+    return render(request, "projects/delete_template.html", context)
 
